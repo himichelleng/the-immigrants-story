4752d0396ee72059684461e1aa18f231118197eb
frogny <frogny@Piet-NY-005640-AP.local>
2017-07-15 15:50:41 -0400

    update index page

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,6 +1,29 @@
 (function(){
 	var domEl = 'timeseries';
-	var data = [{'value': Date.parse(1991)},{'value': Date.parse(2001)}];
+	var data = [{'value': Date.parse(1999)},
+	            {'value': Date.parse(1968)},
+	            {'value': Date.parse(1993)},
+	            {'value': Date.parse(1977)},
+	            {'value': Date.parse(1981)},
+	            {'value': Date.parse(1982)},
+	            {'value': Date.parse(1992)},
+	            {'value': Date.parse(2000)},
+	            {'value': Date.parse(1910)},
+	            {'value': Date.parse(1992)},
+	            {'value': Date.parse(1921)},
+	            {'value': Date.parse(1888)},
+	            {'value': Date.parse(1830)},
+	            {'value': Date.parse(2000)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)},
+	            {'value': Date.parse(2001)}
+	            ];
+
 	var brushEnabled = true;
 	timeseries(domEl, data, brushEnabled);
 })();
